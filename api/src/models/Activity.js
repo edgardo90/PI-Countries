b81e8e95ef7048766027e51213b164474087a626
edgardo90 <edgardo9000@gmail.com>
2022-01-04 21:22:41 -0300

    nuevo

---
+++ api/src/models/Activity.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('activity', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    difficulty:{
+      type: DataTypes.ENUM,
+      allowNull: true,
+      values: ["Beginner", "Amateur", "Normal", "Professional", "Expert"],
+    },
+    duration:{
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    season: {
+      type: DataTypes.ENUM,
+      values:["summer", "autumn" , "winter" , "spring"] // "verano", "otoño" , "invierno" , "primavera"
+    }
+  });
+};
